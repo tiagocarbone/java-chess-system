efddc592b752dcf09a8df774600c718f5eae1283
Tiago Carbone do Nascimento <carbonetiago@id.uff.br>
2023-12-12 09:46:04 -0300

    Starting to implement board and piece

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
+
+
 
     }
 }
